52fabaa8fdebdf5171dad0106c5f903f750e3643
Aflaungos <vimavick@gmail.com>
2024-01-30 11:23:12 -0300

    Added VTS Bypass by TyTyDraco

--- zip_up_kernel.sh
+++ zip_up_kernel.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-
-echo "Copying Kernel Image here"
-cp ${HOME}/android_kernel_samsung_m23xq_common/out/arch/arm64/boot/Image ./
-
-echo "Zip up BoostKernel"
+echo -e "\033[0;32mDeleting last kernel zip\033[0m"
+rm *.zip
+echo -e "\033[0;32mCopying Kernel Image here\033[0m"
+cp ${HOME}/android_kernel_samsung_m23xq/out/arch/arm64/boot/Image ./
+cp ${HOME}/android_kernel_samsung_m23xq/out/arch/arm64/boot/dtbo.img ./dtbo
+cp ${HOME}/android_kernel_samsung_m23xq/out/arch/arm64/boot/dts/vendor/qcom/lagoon.dtb ./dtb
+echo -e "\033[0;32mZip up BoostKernel\033[0m"
 zip -r9 kernel.zip * -x .git *placeholder README.md kernel.zip zip_up_kernel.sh
-
-echo "Deleting Kernel Image"
+echo -e "\033[0;32mDeleting Kernel Image\033[0m"
 rm Image
-
-echo "done"
+rm dtbo
+rm dtb
+echo -e "\033[0;32mDone\033[0m"
